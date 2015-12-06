02ac21679fa40e3440403a2ea34682e1e276d9c1
Parnescu Adrian <parnescu@Parnescus-MBP.home>
2015-12-06 14:36:35 +0000

    map out basic layout of what needs to be tested

--- src/test/ProjectSpec.js
+++ src/test/ProjectSpec.js
@@ -9,4 +9,30 @@ describe("Main test suite", function(){
 			expect(1+1).toEqual(2)
 		})
 	})
+
+	describe("Number Generator class", function(){
+		it("is available", function(){})
+		it("can generate prime numbers", function(){})
+		it("can switch between generation methods", function(){})
+	})
+
+	describe("Number generation methods", function(){
+		describe("Classic method", function(){
+			it("is available", function(){})
+			it("has an id", function(){})
+			it("can generate prime numbers", function(){})
+		})
+
+		describe("Sieve of Eratosthenes method", function(){
+			it("is available", function(){})
+			it("has an id", function(){})
+			it("can generate prime numbers", function(){})
+		})
+
+		describe("Sieve of Atkin method", function(){
+			it("is available", function(){})
+			it("has an id", function(){})
+			it("can generate prime numbers", function(){})
+		})
+	})
 })
